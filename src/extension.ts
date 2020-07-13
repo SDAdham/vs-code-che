856747c5bb7b6e18ccd478ef71867ea6810bc7b4
Adham Sabry <adham.sabry@microcodez.com>
2020-07-13 17:17:03 +1000

    Updated commands

--- src/extension.ts
+++ src/extension.ts
@@ -6,6 +6,16 @@ declare var window: any;
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	try {
+		if (process.env.CHE_WORKSPACE_NAME) {
+			vscode.window.showInformationMessage('VS Code Che has been activated successfully');
+			window.parent.postMessage('hide-navbar');
+		} else {
+			vscode.window.showWarningMessage("This extension should work with Eclipse Che only");
+		}
+	} catch (error) {
+		console.error('Failed to get environment variable', error);
+	}
 
 	// Use the console to output diagnostic information (console.log) and errors (console.error)
 	// This line of code will only be executed once when your extension is activated
@@ -15,34 +25,45 @@ export function activate(context: vscode.ExtensionContext) {
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
 	let disposable = vscode.commands.registerCommand('vs-code-che.showWorkspaceName', () => {
-		if (process.env.CHE_WORKSPACE_NAME) {
-			const workspaceName = process.env.CHE_WORKSPACE_NAME;
-			
-			vscode.window.showInformationMessage(workspaceName);
-		} else {
-			vscode.window.showWarningMessage("This extension should work with Eclipse Che only");
+		try {
+			if (process.env.CHE_WORKSPACE_NAME) {
+				const workspaceName = process.env.CHE_WORKSPACE_NAME;
+				vscode.window.showInformationMessage(workspaceName);
+			} else {
+				vscode.window.showWarningMessage("This extension should work with Eclipse Che only");
+			}
+		} catch (error) {
+			console.error('Failed to get environment variable', error);
 		}
 	});
 
 	context.subscriptions.push(disposable);
 
 	let showNav = vscode.commands.registerCommand('vs-code-che.showNav', () => {
-		if (window) {
-			window.parent.postMessage('show-navbar');
-			vscode.window.showInformationMessage('Showing che navbar');
-		} else {
-			vscode.window.showErrorMessage('This extension is not for VS Code on Desktop');
+		try {
+			if (window) {
+				window.parent.postMessage('show-navbar');
+				vscode.window.showInformationMessage('Showing che navbar');
+			} else {
+				vscode.window.showErrorMessage('This extension is not for VS Code on Desktop');
+			}
+		} catch (error) {
+			console.error('Failed to get parent of the current window', error);
 		}
 	});
 
 	context.subscriptions.push(showNav);
 
 	let hideNav = vscode.commands.registerCommand('vs-code-che.hideNav', () => {
-		if (window) {
-			window.parent.postMessage('hide-navbar');
-			vscode.window.showInformationMessage('Hiding che navbar');
-		} else {
-			vscode.window.showErrorMessage('This extension is not for VS Code on Desktop');
+		try {
+			if (window) {
+				window.parent.postMessage('hide-navbar');
+				vscode.window.showInformationMessage('Hiding che navbar');
+			} else {
+				vscode.window.showErrorMessage('This extension is not for VS Code on Desktop');
+			}
+		} catch (error) {
+			console.error('Failed to get parent of the current window', error);
 		}
 	});
 
